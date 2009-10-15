c16681f8e252232e6073b467bd6997194755887f
Daniel Woodhouse <wodemoneke@gmail.com>
2009-10-15 23:40:35 +0100

    massive changes

--- minesweeper/square.py
+++ minesweeper/square.py
@@ -3,13 +3,14 @@ import random
 import time
 
 class Square(gtk.EventBox):
-    button = gtk.gdk.pixbuf_new_from_file("../images/button.png")
-    mine_pixbuf = gtk.gdk.pixbuf_new_from_file("../images/mine.png")
+    SHARE_DIR = ""
+    button = gtk.gdk.pixbuf_new_from_file(SHARE_DIR + "images/button.png")
+    mine_pixbuf = gtk.gdk.pixbuf_new_from_file(SHARE_DIR + "images/mine.png")
     numbers_pixbuf = []
     for i in range(0, 9):
-        numbers_pixbuf.append(gtk.gdk.pixbuf_new_from_file("../images/%d.png" % i))
-    flag = gtk.gdk.pixbuf_new_from_file("../images/flag.png")
-    question = gtk.gdk.pixbuf_new_from_file("../images/question.png")
+        numbers_pixbuf.append(gtk.gdk.pixbuf_new_from_file(SHARE_DIR + "images/%d.png" % i))
+    flag = gtk.gdk.pixbuf_new_from_file(SHARE_DIR + "images/flag.png")
+    question = gtk.gdk.pixbuf_new_from_file(SHARE_DIR +"images/question.png")
     flag_states = [button, flag, question]
     LEFT_CLICK = 1
     BOTH_CLICK = 2
@@ -33,6 +34,7 @@ class Square(gtk.EventBox):
         self.button_depressed = False
         self.is_covered = True
         self.current_flag_state = 0 # 1 for flag 2 for question mark
+        print self.surrounding_mines
     
     def on_mouse_in(self, widget, event):
         if event.button == Square.LEFT_CLICK or event.button == Square.BOTH_CLICK:
@@ -51,7 +53,7 @@ class Square(gtk.EventBox):
     def on_mouse_released(self, widget, event):
         """Count surrounding mines and uncover or end game if mine"""
         if self.button_depressed:
-            uncover_square()
+            self.uncover_square()
             
     def uncover_square(self):
         self.is_covered = False
@@ -64,6 +66,7 @@ class Square(gtk.EventBox):
         else:
             self.image.set_from_pixbuf(Square.numbers_pixbuf[self.surrounding_mines])
             self.grid.uncover_squares(self.row, self.col)
+            self.grid.uncover_numbers()
             
             
     def flag(self):
@@ -91,25 +94,29 @@ class Grid(object):
         temp_mines = [True] * mines + [False] * (self.rows*self.cols-mines)
         random.shuffle(temp_mines)
         index = 0
-        self.minelist = []
+        minelist = []
         while index < self.rows*self.cols:
-            self.minelist.append(temp_mines[index:index+self.rows])
+            minelist.append(temp_mines[index:index+self.rows])
             index += self.rows
         
         table = gtk.Table(self.rows, self.cols, True)
         table.show()
+        self.minelist = []
         start = time.time()
-        for i, row in enumerate(self.minelist):
+        for i, row in enumerate(minelist):
+            minerow = []
             for j, mine in enumerate(row):
-                square = Square(self, mine, i, j, self.count_surrounding_mines(self.minelist, i, j))
+                square = Square(self, mine, i, j, self.count_surrounding_mines_old_version(minelist, i, j))
                 table.attach(square, left_attach=j, right_attach=j+1,
                              top_attach=i, bottom_attach=i+1)
+                minerow.append(square)
+            self.minelist.append(minerow)
         end = time.time() - start
         print end
         return table
     
-    def count_surrounding_mines(self, minelist, row, col):
-        surrounding = self.return_surrounding_squares(row, col)
+    def count_surrounding_mines_old_version(self, minelist, row, col):
+        surrounding = self.return_surrounding_squares_old_version(row, col)
         total_mines = 0
         for row, col in surrounding:
             if row == -1 or col == -1:
@@ -122,17 +129,56 @@ class Grid(object):
                 pass
         return total_mines
     
-    def return_surrounding_squares(self, row, col):
-         return [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1],
-                 [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]
+    def return_surrounding_squares_old_version(self, row, col):
+        return [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1],
+                [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]
+    
+    def count_surrounding_mines(self, minelist, row, col):
+        surrounding = self.return_surrounding_squares(row, col)
+        total_mines = 0
+        for square in surrounding:
+            if square.is_mine:
+                total_mines += 1
+        return total_mines
+    
+    def return_surrounding_squares(self, row, col, diagonals=True):
+        """Set diagonals to False if uncovering mines"""
+        #XXX should be refactored to accept square objects
+        squares = [[row-1, col], [row, col-1], [row, col+1], [row+1, col]]
+        if diagonals:
+            squares.extend([[row-1, col-1], [row-1, col+1], [row+1, col-1], [row+1, col+1]])
+        surrounding_squares = []
+        for row, col in squares:
+            if row == -1 or col == -1:
+                continue
+            try:
+                surrounding_squares.append(self.minelist[row][col])
+            except IndexError:
+                pass
+        return surrounding_squares
     
     def uncover_squares(self, row, col):
         """row and col are the start square and we work our way outwards"""
-        while True:
-            surrounding = return_surrounding_squares(row, col)
-            for i in surrounding:
-                if self.minelist[i].is_covered and self.minelist[i].surrounding_mines == 0:
-                    self.minelist[i].uncover()
+        surrounding = self.return_surrounding_squares(row, col, diagonals=False)
+        next_batch = []
+        for square in surrounding:
+            if square.is_covered and square.surrounding_mines == 0 and not square.is_mine and square.current_flag_state !=1:
+                    square.uncover_square()
+                    next_batch.append([square.row, square.col])
+        for i, j in next_batch:
+            self.uncover_squares(i, j)
+            
+    def uncover_numbers(self):
+        """This makes sure that there is an uncovered number next to each blank
+        square that is shown"""
+        for row in self.minelist:
+            for square in row:
+                if not square.is_covered and square.surrounding_mines == 0:
+                    surrounding = self.return_surrounding_squares(square.row, square.col)
+                    for item in surrounding:
+                        if item.is_covered and not item.is_mine:
+                            item.uncover_square()
+
     
     def end_game(self):
         print "game over"
@@ -140,6 +186,5 @@ class Grid(object):
     def main_quit(self, *args):
         gtk.main_quit()
         
-Grid(10, 10, 50)
-gtk.main()
+
     
