38bd87adbf4254e11c7bf4c271f7a0e7e0fc7b13
Daniel Woodhouse <wodemoneke@gmail.com>
2009-10-24 04:52:56 +0100

    added smileys [#3 status:resolved]

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import gtk
 import pygtk
 import time
@@ -15,16 +16,24 @@ class MineSweeperGame(object):
             pass
         if len(args) != 3:
             args = [10, 10, 20]
-
+        dir = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+        if os.path.exists(os.path.join(dir, "images")):
+            self.SHARE_DIR = os.path.join(dir, "images") + os.sep
+        else:
+            self.SHARE_DIR = "/usr/share/minesweeper/images/"
+        self.pixbuf_neutral = gtk.gdk.pixbuf_new_from_file(self.SHARE_DIR + "neutral.png")
+        self.pixbuf_shocked = gtk.gdk.pixbuf_new_from_file(self.SHARE_DIR + "shock.png")
+        self.pixbuf_dead = gtk.gdk.pixbuf_new_from_file(self.SHARE_DIR + "lose.png")
+        self.pixbuf_win = gtk.gdk.pixbuf_new_from_file(self.SHARE_DIR + "win.png")
         self.window = gtk.Window()
         self.vbox = gtk.VBox()
         self.window.add(self.vbox)
         #timer, smiley face (new game), mine counter
         self.timer_label = gtk.Label()
         self.smiley_button = gtk.Button()
-        image = gtk.Image()
-        image.set_from_stock(gtk.STOCK_OPEN, gtk.ICON_SIZE_BUTTON)
-        self.smiley_button.set_image(image)
+        self.smiley_image = gtk.Image()
+        self.smiley_image.set_from_pixbuf(self.pixbuf_neutral)
+        self.smiley_button.set_image(self.smiley_image)
         self.mine_counter = gtk.Label()
         self.hbox = gtk.HBox()
         self.hbox.pack_start(self.timer_label)
@@ -49,10 +58,13 @@ class MineSweeperGame(object):
         self.grid.connect("add-flag", self.flagging)
         self.grid.connect("remove-flag", self.flagging)
         self.grid.connect("end-game", self.end_game)
+        self.grid.connect("mouse-in", self.set_face, self.pixbuf_shocked)
+        self.grid.connect("reset-face", self.set_face, self.pixbuf_neutral)
         self.table = self.grid.create_minefield()
         self.vbox.pack_start(self.table)
         self.mine_counter.set_text(str(self.grid.mines))
         self.timer_label.set_text("0")
+        self.set_face(None, self.pixbuf_neutral)
 
     def start_game(self, *args):
         try:
@@ -61,33 +73,44 @@ class MineSweeperGame(object):
             pass
         self.start_time = time.time()
         self.timer = gobject.timeout_add(1000, self.increment_time)
+        
 
+    def set_face(self, widget, pixbuf):
+        self.smiley_image.set_from_pixbuf(pixbuf)
 
-    def end_game(self, *args):
+    def end_game(self, grid, win):
         #if this is called and and square that has a mine is not flagged, it is a loss
         self.grid.disconnect_by_func(self.end_game)
         self.grid.game_over = True
-        gobject.source_remove(self.timer)
-        victory = True if self.mine_counter.get_text() == "0" else False
+        try:
+            gobject.source_remove(self.timer)
+        except AttributeError:
+            pass
         for row in self.grid.minelist:
             for square in row:
-                if square.is_covered and square.is_mine:
-                    square.uncover()
-                elif square.current_flag_state == 1:
-                    square.set_incorrect_flag()
-                    victory = False
-        if victory:
+                if win and square.is_mine:
+                    square.current_flag_state = 0
+                    square.flag()
+                else:
+                    if square.is_covered and square.is_mine:
+                        square.uncover()
+                    elif square.current_flag_state == 1:
+                        square.set_incorrect_flag()
+        if win:
             print 'You win'
+            self.set_face(None, self.pixbuf_win)
         else:
             print 'You lose'
+            self.set_face(None, self.pixbuf_dead)
         #change to dead face
         #show incorrect mine guesses
-        print "game over"
+
 
         
     def flagging(self, grid, value):
-        mines_flagged = int(self.mine_counter.get_text()) + value
-        self.mine_counter.set_text(str(mines_flagged))
+        self.mine_counter.set_text(str(grid.mines_left))
+        if grid.mines_left == 0:
+            pass
         
     def increment_time(self, end=False):
         #XXX needs to end after game
